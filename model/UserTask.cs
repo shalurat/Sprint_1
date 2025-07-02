46aa5c1431c1e997d0fe2d8b685ba14cfc09f1bb
sonalkarne22 <sonalsk2203@gmail.com>
2025-07-02 10:48:36 +0530

    Added data model files

---
+++ model/UserTask.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeCollaborationConsole.Models
+{
+
+    public class UserTask
+    {
+        public int TaskId { get; set; }
+        public int UserId { get; set; }
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public DateTime DueDate { get; set; }
+    }
+}
